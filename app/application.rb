2aa9cb24e170fb3a722d6f62027de5dc8eb8a874
Nicholas Powers <njpowers7915@gmail.com>
2018-05-31 18:06:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split('/items').last
-      if @@items.find {|i| item.name = item_name}
+      if @@items.find {|i| i.name = item_name}
         resp.write item.price
       else
         resp.write "We don't have that item"
