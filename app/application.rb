807c94869273ba4b437d4a4d55bcf6142f1d123b
Nicholas Powers <njpowers7915@gmail.com>
2018-05-31 17:55:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split('/items').last
-      item = @@items.find{|i| i.name == item_name
+      item = @@items.find{|i| i.name == item_name}
       if item == nil
         resp.write "We don't have that item"
         resp.status = 400
