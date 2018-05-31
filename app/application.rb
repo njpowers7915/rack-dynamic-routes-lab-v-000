609de046bcb03d70f28c387710e3281f9e60b5cf
Nicholas Powers <njpowers7915@gmail.com>
2018-05-31 17:58:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,11 +8,13 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split('/items').last
       item = @@items.find{|i| i.name == item_name}
-      if item == nil
+      if @@items.include(item)
+        resp.write "#{item.price}"
+      else
         resp.write "We don't have that item"
         resp.status = 400
       else
-        resp.write "#{item.price}"
+        
       end
     else
       resp.write "Route not found"
